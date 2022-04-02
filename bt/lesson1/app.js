d8efd060d7cb926afd22fa4607b27e1d7cf48e12
AkamaOk <90634432+AkamaOk@users.noreply.github.com>
2022-04-02 22:57:47 +0700

    Add files via upload

---
+++ bt/lesson1/app.js
@@ -0,0 +1,36 @@
+var courses = prompt("Khóa học cần tìm?", "mindX 01" ,  "mindX 02" , "mindX 03" , "mindX 04" );
+if (courses != null) {
+    alert( student );
+} 
+else {
+    alert("Chưa có lớp học này!");
+}
+var courses = new Array(4);
+courses[1] = "mindX 01";
+courses[2] = "mindX 02";
+courses[3] = "mindX 03";
+courses[4] = "mindX 04";
+let student = prompt [ 
+    {
+        name: "NVA",
+        age: 16,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    },
+    {
+        name: "NVB",
+        age: 18,
+        course: ["mindX 01", "mindX 02", "mindX 04"]
+    },
+    {
+        name: "NVC",
+        age: 17,
+        course: ["mindX 01", "mindX 03", "mindX 04"]
+    },
+    {
+        name: "NVD",
+        age: 15,
+        course: ["mindX 01", "mindX 02", "mindX 03"]
+    }
+
+    ]
+    
